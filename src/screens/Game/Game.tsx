53a65c6c3dc882787f06aa06db26d79b89620c1a
Egor-Kozlov <kozlov1998.2010@gmail.com>
2024-02-05 15:03:36 +0100

    Add useTheme hook to Game component

--- src/screens/Game/Game.tsx
+++ src/screens/Game/Game.tsx
@@ -1,10 +1,12 @@
 import {TimerSetup} from '@app/components';
+import {useTheme} from '@app/hooks';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 export const Game = () => {
+  const {colors} = useTheme();
   return (
-    <View>
+    <View style={{backgroundColor: colors.background, flex: 1}}>
       <Text>Game</Text>
       <View>
         <TimerSetup />
